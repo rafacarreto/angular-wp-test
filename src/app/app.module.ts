3b559676435b76058c7d8476186f56583cbdba10
Rafael Carreño <rafacarreto@hotmail.com>
2021-09-10 22:24:24 -0500

    Add bootstrap and ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,6 +31,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    //NgbModule,
     FormsModule,
     ReactiveFormsModule
   ],
